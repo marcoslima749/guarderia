98b917a4a1a22ea0ca7f00e0184e991db64b6910
Marcos Lima <marcoslima749@gmail.com>
2020-06-12 23:48:28 -0300

    Creado el modulo env.js para parsear .env. Hecha una consulta a la base de datos de prueba funcionando correctamente.

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const mysql = require('mysql');
+const envParser = require('./env');
+
+const env = envParser();
 
 //creando la conección a la base de datos
 
+
 const db = mysql.createConnection({
-    host : '',
-    user : '',
-    password: '',
-    database: ''
+    host : env.DB_HOST,
+    user : env.DB_USUARIO,
+    password: env.DB_PASS,
+    database: env.DB_BASE
 });
  
 db.connect((err)=> {
@@ -23,10 +27,12 @@ const app = express();
 //Rutas
 
 app.get('/',(req, res)=> {
+
     db.query('SELECT * FROM embarcaciones',(error, results, fields) => {
         if (error) throw error;
         console.log(results[0].nombre);
     })
+
     res.send('Hola desde el servidor');
 })
 
