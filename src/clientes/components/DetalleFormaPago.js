c7fbadea95086f28e8ca6ca2dde7640b5f9b26ab
Marcos Lima <marcoslima749@gmail.com>
2020-07-30 01:56:00 -0300

    Agregado el styling del detalle de forma de pago

--- src/clientes/components/DetalleFormaPago.js
+++ src/clientes/components/DetalleFormaPago.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Entrada } from '../../shared/components/Entrada';
+import './DetalleFormaPago.css';
 
 export const DetalleFormaPago = ({clases = '', detalle, handleGuardar = ()=>{}, handleDescartar = ()=>{}}) => {
     let [detalleState, setDetalleState] = useState(detalle);
@@ -14,15 +15,14 @@ export const DetalleFormaPago = ({clases = '', detalle, handleGuardar = ()=>{}, 
 
     return(
         <div className={`forma-de-pago__detalle__container ${clases}`}>
-        <div class="forma-de-pago__detalle__inputs">
-            <Entrada handleChange={handleChangeDetalle} name='descripcion' clases='forma-de-pago__detalle__descripcion' value={detalleState.descripcion} placeHolder='Descripción' />
-            <Entrada handleChange={handleChangeDetalle} name='numero' clases='forma-de-pago__detalle__numero' value={detalleState.numero} placeHolder='Número' />
-        </div>
-        <div className='forma-de-pago__detalle__botones'>
-            <button onClick={()=>handleGuardar(detalleState)} className='forma-de-pago__detalle__botones__guardar simple-hover'>Guardar</button>
-            <button onClick={handleDescartar} className='forma-de-pago__detalle__botones__descartar simple-hover'>Descartar</button>
-        </div>
-
+            <div class="forma-de-pago__detalle__inputs">
+                <Entrada handleChange={handleChangeDetalle} name='descripcion' clases='forma-de-pago__detalle__descripcion' value={detalleState.descripcion} placeHolder='Descripción' />
+                <Entrada handleChange={handleChangeDetalle} name='numero' clases='forma-de-pago__detalle__numero' value={detalleState.numero} placeHolder='Número' />
+            </div>
+            <div className='forma-de-pago__detalle__botones'>
+                <button onClick={()=>handleGuardar(detalleState)} className='forma-de-pago__detalle__boton forma-de-pago__detalle__boton__guardar simple-hover'>Guardar</button>
+                <button onClick={handleDescartar} className='forma-de-pago__detalle__boton forma-de-pago__detalle__boton__descartar simple-hover'>Descartar</button>
+            </div>
         </div>
     )
 }
